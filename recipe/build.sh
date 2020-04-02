5db14a3ebc45d9d434ce5913fccf1d0aed9cd20d
Howard Butler <howard@hobu.co>
2020-04-02 00:21:21 -0500

    windows fixes

---
+++ recipe/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
+if [ "$(uname)" == "Linux" ]; then
+   export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
+fi
+
+${PYTHON} -m pip install . -vv
