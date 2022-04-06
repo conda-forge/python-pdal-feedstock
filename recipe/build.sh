cb44a373fe3efc297ac865170ddb44d339a2e574
Howard Butler <hobu.inc@gmail.com>
2022-04-06 16:11:52 -0500

    Revert to installing python-plugin

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,11 +9,25 @@ export CXXFLAGS
 
 if [ "$target_platform" = "osx-arm64" ]; then
   export SKBUILD_CONFIGURE_OPTIONS=${CMAKE_ARGS/CMAKE_INSTALL_PREFIX/CMAKE_INSTALL_PREFIX_BAK}
+  export SKBUILD_CONFIGURE_OPTIONS="-DPdal_DIR=$PREFIX/lib/cmake/PDAL -DPython3_EXECUTABLE=$PREFIX/bin/python "
   export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
 
 
 ${PYTHON} -m pip install . -v
+
+mkdir plugins && cd plugins
+curl -OL https://files.pythonhosted.org/packages/66/e6/377c308a7f7d7f2a97008721e83061308a56e23f6be3b07989d44a4cfa9a/pdal-plugins-1.2.0.tar.gz
+tar xvf pdal-plugins-1.2.0.tar.gz
+cd pdal-plugins-1.2.0
+
+#if [ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]; then
+#rm $BUILD_PREFIX/lib/libpdal*
+#rm $BUILD_PREFIX/lib/libpython*
+#fi
+
+${PYTHON} -m pip install . -v
+cd ../..
 
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
