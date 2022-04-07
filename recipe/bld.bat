6232026f7185f8f392c175154d5f624c37450c19
Howard Butler <hobu.inc@gmail.com>
2022-04-07 10:08:50 -0500

    direct install (#69)
    * direct install
    
    * more flail
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.06.15.06.15
    
    * pin_compatible
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.06.15.06.15
    
    * pybind11
    
    * pypi index URL
    
    * actuall pypi index URL
    
    * --extra-index-url
    
    * lint
    
    * manual installs
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.06.15.06.15
    
    * need cmake for build
    
    * depends
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.06.19.57.51
    
    * clean up host libraries when cross compiling
    
    * CMAKE_SYSTEM_PREFIX_PATH
    
    * set Pdal_DIR and Python3_EXECUTABLE
    
    * ditch pdal-plugins
    
    * dont need curl
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.06.19.57.51
    
    * Revert to installing python-plugin
    
    * win build
    
    * pass build args to pip
    
    * try PYTHON_EXECUTABLE
    
    * MNT: Re-rendered with conda-build 3.21.8, conda-smithy 3.19.0, and conda-forge-pinning 2022.04.06.21.38.29
    
    * Python_EXECUTABLE
    
    * Debug
    
    * Update build.sh
    
    * Update build.sh
    
    * Update build.sh
    
    * Fix PDAL_DIR
    
    * Update build.sh
    
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,17 @@
 set CMAKE_GENERATOR=Ninja
 %PYTHON% setup.py install -vv -- -DPython3_EXECUTABLE="%PYTHON%"
 
+
+
+mkdir plugins
+cd plugins
+curl -OL https://files.pythonhosted.org/packages/18/6c/1077b11602cac1f11e0c80a7ca8008bf21b8d2b141c6022b56306ca407af/pdal-plugins-1.1.0.tar.gz
+tar xvf pdal-plugins-1.1.0.tar.gz
+cd pdal-plugins-1.1.0
+
+%PYTHON% -m pip install . -v
+cd ../..
+
 set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
 set DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d
 mkdir %ACTIVATE_DIR%
--- recipe/build.sh
+++ recipe/build.sh
@@ -8,13 +8,26 @@ CXXFLAGS="${CXXFLAGS/-std=c++11/}"
 export CXXFLAGS
 
 if [ "$target_platform" = "osx-arm64" ]; then
-  export SKBUILD_CONFIGURE_OPTIONS=${CMAKE_ARGS/CMAKE_INSTALL_PREFIX/CMAKE_INSTALL_PREFIX_BAK}
   export CMAKE_OSX_ARCHITECTURES="arm64"
 fi
 
+# scikit-build only passes PYTHON_EXECUTABLE and doesn't pass Python3_EXECUTABLE
+export CMAKE_ARGS="${CMAKE_ARGS} -DPython3_EXECUTABLE=$PREFIX/bin/python -DPython_EXECUTABLE=$PREFIX/bin/python -DPDAL_DIR=$PREFIX -LAH"
 
-set CMAKE_GENERATOR=Ninja
 ${PYTHON} -m pip install . -v
+
+mkdir plugins && cd plugins
+curl -OL https://files.pythonhosted.org/packages/66/e6/377c308a7f7d7f2a97008721e83061308a56e23f6be3b07989d44a4cfa9a/pdal-plugins-1.2.0.tar.gz
+tar xvf pdal-plugins-1.2.0.tar.gz
+cd pdal-plugins-1.2.0
+
+#if [ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]; then
+#rm $BUILD_PREFIX/lib/libpdal*
+#rm $BUILD_PREFIX/lib/libpython*
+#fi
+
+${PYTHON} -m pip install . -vv
+cd ../..
 
 ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
 DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
