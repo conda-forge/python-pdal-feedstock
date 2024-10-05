a254f71b062a4fd9419e2546d85ef49fc1ad415c
Howard Butler <hobu.inc@gmail.com>
2024-10-04 20:15:11 -0500

    Always set PDAL_DRIVER_PATH for activate.sh (#129)

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -2,14 +2,8 @@
 
 # Store existing PDAL env vars and set to this conda env
 # so other PDAL installs don't pollute the environment
-
 if [[ -n "$PDAL_DRIVER_PATH" ]]; then
     export _CONDA_SET_PDAL_PYTHON_DRIVER_PATH=$PDAL_DRIVER_PATH
 fi
 
-if [[ ! -z "$PLUGIN_DIR_PATH" ]]; then
-    export PDAL_DRIVER_PATH=$_CONDA_SET_PDAL_PYTHON_DRIVER_PATH:@PLUGIN_DIR_PATH@
-fi
-
-
-
+export PDAL_DRIVER_PATH=$_CONDA_SET_PDAL_PYTHON_DRIVER_PATH:@PLUGIN_DIR_PATH@
