ecb9aa9af6e53bf9640abb8df324b516b3c633d6
Alkri <42676563+Alkri@users.noreply.github.com>
2026-02-27 00:25:23 +0800

    fix PDAL_DRIVER_PATH logic in powershell activation script (#147)
    * fix PDAL_DRIVER_PATH logic in powershell activation script
    
    * MNT: Re-rendered with conda-smithy 3.55.1 and conda-forge-pinning 2026.02.26.09.03.2
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/scripts/activate.ps1
+++ recipe/scripts/activate.ps1
@@ -1,8 +1,12 @@
 if ($ENV:PDAL_DRIVER_PATH) {
-    $ENV:_CONDA_SET_PDAL_DRIVER_PATH=$ENV:PDAL_DRIVER_PATH
+    $ENV:_CONDA_SET_PDAL_DRIVER_PATH = $ENV:PDAL_DRIVER_PATH
 }
 
-$ENV:PDAL_DRIVER_PATH="$ENV:PDAL_DRIVER_PATH;$ENV:CONDA_PREFIX\Lib\site-packages\bin\;$ENV:CONDA_PREFIX\bin"
-if (-Not (Test-Path $ENV:PDAL_DRIVER_PATH) ) {
-     Remove-Item ENV:PDAL_DRIVER_PATH
+$pdal_paths = @()
+if ($ENV:PDAL_DRIVER_PATH) {
+    $pdal_paths += $ENV:PDAL_DRIVER_PATH
 }
+$pdal_paths += "$ENV:CONDA_PREFIX\Lib\site-packages\bin"
+$pdal_paths += "$ENV:CONDA_PREFIX\bin"
+
+$ENV:PDAL_DRIVER_PATH = $pdal_paths -join ";"
